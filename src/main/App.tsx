103d5bd929479b7bf729db6446598b0ebc128746
andrew regoli <andrewregoli1@gmail.com>
2024-09-02 21:09:07 +1000

    revert

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -2,34 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 import { LoanComponent } from '../loananalysiscomponent/loananaylsis';
 import './App.css';
-import { Amplify } from 'aws-amplify';
-import { generateClient } from 'aws-amplify/api';
-import config from '../amplifyconfiguration.json';
-import { createTodo, updateTodo, deleteTodo } from '../graphql/mutations';
-import { listTodos } from '../graphql/queries';
-Amplify.configure(config);
-
-const client = generateClient();
 function App() {
-
-  async function tst(){
-    const result = await client.graphql({
-      query: createTodo,
-      variables: {
-        input: {
-          name: 'My first todo!'
-        }
-      }
-    });
-  }
-  tst();
-
-  async function tt(){
-    const result = await client.graphql({ query: listTodos });
-console.log(result);
-  }
-  tt();
-
   return (
     <BrowserRouter>          
     <Navbar/>
@@ -41,6 +14,5 @@ console.log(result);
     </BrowserRouter>
   );
 }
-
 
 export default App;
