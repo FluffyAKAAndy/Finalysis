061a082fcd4289223d8df3b36fb0760f16a94895
andrew regoli <andrewregoli1@gmail.com>
2024-09-02 21:04:22 +1000

    failedbuild

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -6,6 +6,7 @@ import { Amplify } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
 import config from '../amplifyconfiguration.json';
 import { createTodo, updateTodo, deleteTodo } from '../graphql/mutations';
+import { listTodos } from '../graphql/queries';
 Amplify.configure(config);
 
 const client = generateClient();
@@ -21,7 +22,14 @@ function App() {
       }
     });
   }
-  
+  tst();
+
+  async function tt(){
+    const result = await client.graphql({ query: listTodos });
+console.log(result);
+  }
+  tt();
+
   return (
     <BrowserRouter>          
     <Navbar/>
